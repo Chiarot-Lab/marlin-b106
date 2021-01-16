a52f04d9dde731d967773e057594b814d78c6062
Joe Prisaznuk <jprisaz1@binghamton.edu>
2021-01-16 13:50:59 -0500

    changed limits after homing

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1122,12 +1122,12 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 120
-#define Y_BED_SIZE 120
+#define X_BED_SIZE 122
+#define Y_BED_SIZE 122
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS 1
-#define Y_MIN_POS 1
+#define X_MIN_POS -3
+#define Y_MIN_POS -3
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
